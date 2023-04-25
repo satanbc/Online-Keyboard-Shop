2fa30f9939fc46badb38e9762a030dc48797ff2b
satanbc <satanbbc@gmail.com>
2023-04-25 18:54:47 +0300

    Initial commit

---
+++ src/main/java/com/ecommerce/year2_sem2_project/Year2Sem2ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.year2_sem2_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Year2Sem2ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Year2Sem2ProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecommerce/year2_sem2_project/Year2Sem2ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.year2_sem2_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Year2Sem2ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

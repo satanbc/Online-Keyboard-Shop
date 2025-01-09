997140aaf6ce91d2b98f1d9c91ec3c6f3e74d56e
satanbc <satanbbc@gmail.com>
2025-01-09 13:14:30 +0200

    SQL dump update

--- ecommerce_database/ecom.sql
+++ ecommerce_database/ecom.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: ecommerce_db
 -- ------------------------------------------------------
--- Server version	8.0.30
+-- Server version	8.0.40
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -33,7 +33,7 @@ CREATE TABLE `ordered_items` (
   KEY `product_id` (`product_id`),
   CONSTRAINT `ordered_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
   CONSTRAINT `ordered_items_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,7 +42,6 @@ CREATE TABLE `ordered_items` (
 
 LOCK TABLES `ordered_items` WRITE;
 /*!40000 ALTER TABLE `ordered_items` DISABLE KEYS */;
-INSERT INTO `ordered_items` VALUES (1,1,4,1,179.00),(2,1,1,1,159.00),(3,2,1,1,159.00),(4,2,2,1,169.00),(5,3,1,1,159.00),(6,4,1,1,159.00),(7,5,1,1,159.00),(8,7,1,1,159.00),(9,8,2,1,169.00),(10,9,2,1,169.00),(11,10,1,1,159.00),(12,11,1,1,159.00),(13,12,1,1,159.00),(14,13,2,1,169.00),(15,14,2,1,169.00),(16,14,2,1,169.00),(17,15,1,1,159.00),(18,16,2,1,169.00),(19,17,1,1,159.00),(20,19,2,1,169.00),(21,20,1,1,159.00),(22,21,1,1,159.00),(23,22,1,1,159.00),(24,23,1,1,159.00),(25,24,1,1,159.00),(26,25,1,1,159.00),(27,26,1,1,159.00),(28,27,1,1,159.00),(29,28,1,1,159.00),(30,29,3,1,230.00),(31,29,1,1,159.00),(32,30,1,1,159.00),(33,30,1,1,159.00);
 /*!40000 ALTER TABLE `ordered_items` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -61,7 +60,7 @@ CREATE TABLE `orders` (
   `total_price` decimal(10,2) DEFAULT NULL,
   `order_date` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -70,7 +69,6 @@ CREATE TABLE `orders` (
 
 LOCK TABLES `orders` WRITE;
 /*!40000 ALTER TABLE `orders` DISABLE KEYS */;
-INSERT INTO `orders` VALUES (1,'Матвей Сергеевич Володовский','matrundelpro@gmail.com','ул. Стадионная 6А',338.00,'2023-05-07'),(2,'Матвей Сергеевич Володовский','matrundelpro@gmail.com','ул. Стадионная 6А',328.00,'2023-05-07'),(3,'Матвей Сергеевич Володовский','matrundelpro@gmail.com','ул. Стадионная 6А',159.00,'2023-05-07'),(4,'Матвей Сергеевич Володовский','matrundelpro@gmail.com','ул. Стадионная 6А',159.00,'2023-05-13'),(5,'Матвей Сергеевич Володовский','matrundelpro@gmail.com','ул. Стадионная 6А',159.00,'2023-05-13'),(6,'Матвей Сергеевич Володовский','matrundelpro@gmail.com','ул. Стадионная 6А',0.00,'2023-05-13'),(7,'Matvii Volodovskyi','satanbbc@gmail.com','27 Merry Lane NP000424285',159.00,'2023-05-13'),(8,'Matvii Volodovskyi','satanbbc@gmail.com','27 Merry Lane NP000424285',169.00,'2023-05-13'),(9,'Matvii Volodovskyi','satanbbc@gmail.com','27 Merry Lane NP000424285',169.00,'2023-05-13'),(10,'Matvii Volodovskyi','satanbbc@gmail.com','27 Merry Lane NP000424285',159.00,'2023-05-13'),(11,'Matvii Volodovskyi','satanbbc@gmail.com','27 Merry Lane NP000424285',159.00,'2023-05-13'),(12,'Matvii Volodovskyi','satanbbc@gmail.com','27 Merry Lane NP000424285',159.00,'2023-05-13'),(13,'Matvii Volodovskyi','satanbbc@gmail.com','27 Merry Lane NP000424285',169.00,'2023-05-13'),(14,'Matvii Volodovskyi','satanbbc@gmail.com','27 Merry Lane NP000424285',338.00,'2023-05-13'),(15,'Matvii Volodovskyi','satanbbc@gmail.com','27 Merry Lane NP000424285',159.00,'2023-05-13'),(16,'Matvii Volodovskyi','satanbbc@gmail.com','27 Merry Lane NP000424285',169.00,'2023-05-14'),(17,'Matvii Volodovskyi','satanbbc@gmail.com','27 Merry Lane NP000424285',159.00,'2023-05-14'),(18,'Matvii Volodovskyi','satanbbc@gmail.com','27 Merry Lane NP000424285',0.00,'2023-05-14'),(19,'Matvii Volodovskyi','satanbbc@gmail.com','27 Merry Lane NP000424285',169.00,'2023-05-14'),(20,'Matvii Volodovskyi','satanbbc@gmail.com','27 Merry Lane NP000424285',159.00,'2023-05-14'),(21,'Matvii Volodovskyi','satanbbc@gmail.com','27 Merry Lane NP000424285',159.00,'2023-05-14'),(22,'Matvii Volodovskyi','satanbbc@gmail.com','27 Merry Lane NP000424285',159.00,'2023-05-14'),(23,'Matvii Volodovskyi','satanbbc@gmail.com','27 Merry Lane NP000424285',159.00,'2023-05-14'),(24,'Matvii Volodovskyi','satanbbc@gmail.com','27 Merry Lane NP000424285',159.00,'2023-05-14'),(25,'Matvii Volodovskyi','satanbbc@gmail.com','27 Merry Lane NP000424285',159.00,'2023-05-14'),(26,'Matvii Volodovskyi','satanbbc@gmail.com','27 Merry Lane NP000424285',159.00,'2023-05-14'),(27,'Matvii Volodovskyi','satanbbc@gmail.com','27 Merry Lane NP000424285',159.00,'2023-05-14'),(28,'Matvii Volodovskyi','satanbbc@gmail.com','27 Merry Lane NP000424285',159.00,'2023-05-14'),(29,'Matvii Volodovskyi','satanbbc@gmail.com','27 Merry Lane NP000424285',389.00,'2023-05-14'),(30,'Матвей Сергеевич Володовский','matrundelpro@gmail.com','ул. Стадионная 6А',318.00,'2023-05-14');
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -89,7 +87,7 @@ CREATE TABLE `products` (
   `stock` int NOT NULL,
   `image_url` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -111,4 +109,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-05-15  0:35:13
+-- Dump completed on 2025-01-09 13:13:31
